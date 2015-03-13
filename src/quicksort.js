3350f82df0b09f9c0df42086afa1ee4e79a3cdd6
wuxizhe <georgewing@gmail.com>
2015-03-13 10:38:48 +0800

    fix: removed `key` param, for keep code clean

--- src/quicksort.js
+++ src/quicksort.js
@@ -1,28 +1,28 @@
-(function() {
+(function(window) {
 
-    function quicksort(arr, key) {
+    'use strict';
+
+    function quicksort(arr) {
         if (arr.length < 1) {
             return arr;
         }
 
         var pivotIndex = Math.floor(arr.length / 2);
         var pivot = arr.splice(pivotIndex, 1)[0];
-        var pivotValue = pivot[key];
 
         var left = [];
         var right = [];
 
         for (var i = 0; i < arr.length; i+=1) {
             var arri = arr[i];
-            var arriValue = arri[key];
-            if (arriValue < pivotValue) {
+            if (arri < pivot) {
                 left.push(arr[i]);
             } else {
                 right.push(arr[i]);
             }
         }
 
-        return quicksort(keft, key).concat([pivot], quicksort(right, key));
+        return quicksort(left).concat([pivot], quicksort(right));
     }
 
-})();
+})(this);
